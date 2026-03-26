4dddf1cad5a86412afbacf4b3ad104d06472ed4b
sec0nd-n4ture <a644cr@gmail.com>
2026-03-26 21:06:17 +0300

    increment package version

--- setup.py
+++ setup.py
@@ -2,12 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='soldat_extmod_api',
-    version='0.1.9',
+    version='0.2.0',
     packages=find_packages(),
     install_requires=[
         'keystone_engine>=0.9.2',
         'pefile>=2023.2.7',
-        'pynput>=1.7.6',
         'pywin32>=306'
     ],
     package_data={'': ['**/*.asm']},
