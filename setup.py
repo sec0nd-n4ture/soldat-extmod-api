b88e185b0ec9176d682fcdc9744a353d7d333593
sec0nd-n4ture <a644cr@gmail.com>
2026-04-29 23:49:31 +0300

    increment package version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='soldat_extmod_api',
-    version='0.2.5',
+    version='0.2.6',
     packages=find_packages(),
     install_requires=[
         'keystone_engine>=0.9.2',
