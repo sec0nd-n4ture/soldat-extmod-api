3553c80f2c7afa626615b27f2dd140da9aaf41e1
sec0nd-n4ture <a644cr@gmail.com>
2026-06-22 02:52:23 +0300

    add convenience methods for read operations

--- soldat_extmod_api/soldat_bridge.py
+++ soldat_extmod_api/soldat_bridge.py
@@ -1,6 +1,7 @@
 from soldat_extmod_api.interprocess_utils.process_bridge import ProcessBridge
 from win32.lib.win32con import MEM_COMMIT, MEM_RESERVE, PAGE_EXECUTE_READ, PAGE_READWRITE, MEM_RELEASE
-
+from typing import Any
+import struct
 
 WAIT_FOR_SINGLE_OBJECT_TIMEOUT = 2000 #ms
 PROCESS_ALL_ACCESS = 0x1F0FFF
@@ -11,3 +12,32 @@ class SoldatBridge(ProcessBridge):
     """
     def __init__(self):
         super().__init__(PROCESS_ALL_ACCESS, "Soldat")
+
+    def read_delphi_raw_string(self, string_address: int) -> bytes:
+        if not string_address:
+            return b""
+        str_length = self.read_value(string_address - 4, "i")
+        if str_length <= 0:
+            return b""
+        return self.read(string_address, str_length)
+
+    def read_delphi_utf8_string(self, string_address: int) -> str:
+        raw_bytes = self.read_delphi_raw_string(string_address)
+        return raw_bytes.decode("utf-8", errors="replace")
+
+    def read_delphi_utf16_string(self, string_address: int) -> str:
+        raw_bytes = self.read_delphi_raw_string(string_address)
+        return raw_bytes.decode("utf-16", errors="replace")
+
+    def read_ptr(self, address: int) -> int:
+        return self.read_value(address, "I") or 0
+
+    def read_value(self, address: int, struct_format: str) -> Any:
+        if not address:
+            return None
+
+        num_bytes = struct.calcsize(struct_format)
+        raw_bytes = self.read(address, num_bytes)
+
+        unpacked = struct.unpack(struct_format, raw_bytes)
+        return unpacked[0] if len(unpacked) == 1 else unpacked
