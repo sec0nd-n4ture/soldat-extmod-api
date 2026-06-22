3acad0218774546d2ef46cc166bd26bf0aa40e7f
sec0nd-n4ture <a644cr@gmail.com>
2026-06-23 02:18:50 +0300

    refactor map graphics struct

--- soldat_extmod_api/graphics_helper/map_graphics_struct.py
+++ soldat_extmod_api/graphics_helper/map_graphics_struct.py
@@ -11,41 +11,16 @@ class TMapGraphics:
         return self.api.soldat_bridge.read_delphi_utf8_string(name_ptr)
 
     def get_vertex_buffer(self) -> int:
-        return int.from_bytes(
-            self.api.soldat_bridge.read(
-                self.base_address+0x10, 4
-            ), 
-            "little"
-        )
+        return self.api.soldat_bridge.read_ptr(self.base_address+0x10)
 
     def get_polys_front(self) -> int:
-        return int.from_bytes(
-            self.api.soldat_bridge.read(
-                self.base_address+0xA4, 4
-            ), 
-            "little"
-        )
+        return self.api.soldat_bridge.read_ptr(self.base_address+0xA4)
 
     def get_polys_front_count(self) -> int:
-        return int.from_bytes(
-            self.api.soldat_bridge.read(
-                self.base_address+0xA8, 4
-            ), 
-            "little"
-        )
+        return self.api.soldat_bridge.read_value(self.base_address+0xA8, "I")
 
     def get_polys_back(self) -> int:
-        return int.from_bytes(
-            self.api.soldat_bridge.read(
-                self.base_address+0x9C, 4
-            ), 
-            "little"
-        )
+        return self.api.soldat_bridge.read_ptr(self.base_address+0x9C)
 
     def get_polys_back_count(self) -> int:
-        return int.from_bytes(
-            self.api.soldat_bridge.read(
-                self.base_address+0xA0, 4
-            ), 
-            "little"
-        )
+        return self.api.soldat_bridge.read_value(self.base_address+0xA0, "I")
